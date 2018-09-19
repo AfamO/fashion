cde312f05b44a8b39e81b6cc8f3d23b13379fe37
AfamO <so@tivasgroup.com>
2018-09-19 10:16:28 +0100

    Rearranged the passedPhysicalinspectionemail.html picture postion

--- src/main/java/com/longbridge/services/implementations/OrderServiceImpl.java
+++ src/main/java/com/longbridge/services/implementations/OrderServiceImpl.java
@@ -677,7 +677,7 @@ public class OrderServiceImpl implements OrderService {
             Orders orders=orderRepository.findOne(orderReqDTO.getId());
             Date date = new Date();
             orders.setUpdatedOn(date);
-            if(orders.getDeliveryStatus().equalsIgnoreCase("A")){
+            //if(orders.getDeliveryStatus().equalsIgnoreCase("A")){
                 TransferInfo transferInfo = transferInfoRepository.findByOrders(orders);
                if(transferInfo == null){
                  return "nopayment";
@@ -698,7 +698,7 @@ public class OrderServiceImpl implements OrderService {
                     orders.setUpdatedBy(user.getEmail());
                     orderRepository.save(orders);
                     sendEmailAsync.sendPaymentConfEmailToUser(customer,orders.getOrderNum());
-                }
+               // }
         }catch (Exception ex){
             ex.printStackTrace();
             throw new WawoohException();
